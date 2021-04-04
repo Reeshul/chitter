7a5a319bc7b70c20bdaf8cb641736535be43b2a3
Reeshul <reeshul@hotmail.com>
2021-04-04 11:30:31 +0100

    US2: Refactoring

--- spec/feature/reading_peeps_spec.rb
+++ spec/feature/reading_peeps_spec.rb
@@ -1,10 +1,14 @@
-feature "when reading peeps" do
-  scenario "they should be shown in reverse chronological order" do
-    Peep.create(message: "Peep test message 1")
-    Peep.create(message: "Peep test message 2")
-    Peep.create(message: "Peep test message 3")
-    visit("/")
-    page.find("#peeps_container div:first-child", text: "Peep test message 3")
-    page.find("#peeps_container div:last-child", text: "Peep test message 1")
+# frozen_string_literal: true
+
+describe Peep do
+  describe 'when reading peeps' do
+    it 'they should be shown in reverse chronological order' do
+      described_class.create(message: 'Peep test message 1')
+      described_class.create(message: 'Peep test message 2')
+      described_class.create(message: 'Peep test message 3')
+      visit('/')
+      page.find('#peeps_container div:first-child', text: 'Peep test message 3')
+      page.find('#peeps_container div:last-child', text: 'Peep test message 1')
+    end
   end
 end
